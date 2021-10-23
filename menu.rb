d628b2a031d8bdd8902bf6a262d1abd456bf21bf
Taylor Birchem <taylorbirchem@Taylors-MacBook-Pro.local>
2021-10-23 08:53:01 -0700

    menu.rb

---
+++ menu.rb
@@ -0,0 +1,9 @@
+#writen by Taylor Birchem (10/19/2021)
+#last modified by Taylor Birchem (10/19/2021)
+
+class Bunnies
+  #input for bunnies
+    puts "How many bunnies would you like: "
+    bunnies = gets.chomp
+    puts(bunnies + " Bunnies")
+  end

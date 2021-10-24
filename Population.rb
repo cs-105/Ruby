d2d081b6431d3992800078c36af77f8e949fc07b
jchiplin <60380336+jchiplin@users.noreply.github.com>
2021-10-23 17:10:45 -0700

    Update Population.rb
    Fixed comments

--- Population.rb
+++ Population.rb
@@ -71,36 +71,18 @@ while(quitAnswer != 'q')
   end
 end
 
+#To Do:
 
 #Need it to append startingPop to the array
 #Then append other pops to the array
 
-  #print current population after each progressed month
 
-  #Psuedocode:
-  #currentPopulation takes the last month's population and uses the population growth formula to get a new population total
-  #lastPopulation is the previous month's population
-  
-  #on month 0
-  #startingPopulation = userInput()
-  #populationValues.append(startingPopulation)
-  #currentPopulation = startingPopulation
-  
-  #for each month,
-  #currentPopulation = lastPopulation * ((2.71828) ^ 0.1)
-  
-  #month number is equal to the index of the array
+#Specifications:
 
-  #Possible edge cases:
-    #Within setCurrentPopulation, may need error catching with months to not be less than 0
-
-
-
-#To Do:
-  #1 tile, 1 species for now, but try to allow for expansion to more species and tiles later
-  #Create an object that contains data for rabbits that gets put into the array at every month 
-  #Array gets updated and new populations put into the array for a new month
+#1 tile, 1 species for now, but try to allow for expansion to more species and tiles later
+#Create an object that contains data for rabbits that gets put into the array at every month 
+#Array gets updated and new populations put into the array for a new month
   
-  #Figure out how the user can input starting population and increment the months in the game
+#Figure out how the user can input starting population and increment the months in the game
   
-  #May need to add get methods for each variable
+#May need to add get methods for each variable

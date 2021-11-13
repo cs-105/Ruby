7e4331a6c352f5953c54c02df7e3864a4df7cfa9
Joshua Cross <jcross@westmont.edu>
2021-11-13 14:33:15 -0800

    created support for mountain tiles and expanded berry bushes

--- BerryBush.rb
+++ BerryBush.rb
@@ -1,17 +1,71 @@
 #writen by Joshua Cross (11/7/2021)
-#last modified by Joshua Cross (11/7/2021)
+#last modified by Joshua Cross (11/13/2021)
 
 
 class BerryBush
-  #globalPopulation is a count of how many berry bushes there are in the entire simulation (note: not how many berry bush objects, but the sum of each berry bush objects local population)
+  #DEFAULTFOODRATE is the default value for foodRate
+  #DEFAULTREPRODUCTIONRATE is the default value for reproductionRate
+  #DEFAULTPOPCAP is effectively the default value for @popCap
+  #MOUNTAINPOPCAP is the value for @popcap if mountain is true
+  DEFAULTFOODRATE = 2
+  DEFAULTREPRODUCTIONRATE = 2
+  DEFAULTPOPCAP = 1000
+  MOUNTAINPOPCAP = 500
+  #@@globalPopulation is a count of how many berry bushes there are in the entire simulation (note: not how many berry bush objects, but the sum of each berry bush object's local population)
   @@globalPopulation = 0
-  #foodRate is the rate at which the berry bushes produce food (NOTE: I MAY REWORK THIS TO BE CLASS VARIABLE IN THE NEAR FUTURE SO CONSIDER THIS PORTION OF THE API VOLITILE FOR THE TIME BEING.)
-  #reproductionRate is the rate at which the berry bushes reproduce (NOTE: I MAY REWORK THIS TO BE CLASS VARIABLE IN THE NEAR FUTURE SO CONSIDER THIS PORTION OF THE API VOLITILE FOR THE TIME BEING.)
-  #localPopulation is the number of berry bushes in the area associated with an instance of the BerryBush class
-  def initialize(foodRate, reproductionRate, localPopulation)
+  #@foodRate is the rate at which the berry bushes produce food 
+  #@reproductionRate is the rate at which the berry bushes reproduce 
+  #@localPopulation is the number of berry bushes in the area associated with an instance of the BerryBush class
+  #mountain should be true if and only if the tile that this BerryBush object exists in is mountainous
+  def initialize(localPopulation, foodRate = DEFAULTFOODRATE, reproductionRate = DEFAULTREPRODUCTIONRATE, mountain = false)
     @foodRate = foodRate
     @reproductionRate = reproductionRate
-    @localPopulation = localPopulation
-    @@globalPopulation = @@globalPopulation + localPopulation
+    puts("mountain is " + mountain.to_s)
+    if(mountain)
+      @popCap = MOUNTAINPOPCAP
+    else
+      @popCap = DEFAULTPOPCAP
+    end
+    
+    if(@popCap > localPopulation)
+      @localPopulation = localPopulation
+      puts("@popCap > localPopulation")
+    else
+      @localPopulation = @popCap
+      puts("@popCap <= localPopulation")
+    end
+    #puts("LOCAL POPULATION IS " + @localPopulation.to_s)
+    @@globalPopulation = @@globalPopulation + @localPopulation
   end
+
+  #this is a very rough prototype that will be updated!
+  def reproduction
+    if(@localPopulation > 1)
+      @@globalPopulation = @@globalPopulation + (@localPopulation * @reproductionRate).floor
+      oldPop = @localPopulation
+      @localPopulation = @localPopulation + (@localPopulation * @reproductionRate).floor
+      if(@localPopulation > @popCap)
+        enforcer = @localPopulation - @popCap
+        @localPopulation = @localPopulation - enforcer
+        @@glocalPopulation = @@globalPopulation - enforcer
+      end
+    end
+  end
+
+  def getLocalPopulation
+    return(@localPopulation)
+  end
+
+  def getGlobalPopulation
+    return(@@globalPopulation)
+  end
+
+  def getFoodRate
+    return(@foodRate)
+  end
+
+  def getPopCap
+    return(@popCap)
+  end
+
 end
--- Land.rb
+++ Land.rb
@@ -1,14 +1,34 @@
 #writen by Joshua Cross (10/17/2021)
-#last modified by Joshua Cross (11/6/2021)
+#last modified by Joshua Cross (11/12/2021)
+
+
+require_relative 'BerryBush.rb'
 
 class Land
+  #@@mountainThreshold is the minumum elevation for a tile to be considered a mountain tile
+  @@mountainThreshold = 10
   #create a Land object. These objects will be the subunits of the map
   #takes species and elevation as arguements. species should be an array containing species objects (or at least members of children classes of species) while elevation should be (some sort of number, probably an int but that decision still needs to be made)
-  #species is an array containing decending from the species class
-  #elevation is a number (type still tbd) that reflects the elevation of the tile.
-  def initialize(species, elevation)
+  #@species is an array containing decending from the species class
+  #@elevation is a number (type still tbd) that reflects the elevation of the tile.
+  #@mountainous is true if elevation is greater than or equal to the mountainThreshold, false otherwise
+  #@berryBushes is a BerryBush object that represents all of the berry bushes in this tile 
+  def initialize(species, elevation, berryBushes = 0)
+    puts("berryBushes: " + berryBushes.to_s)
     @species = species
     @elevation = elevation
+    
+    
+    if(@elevation >= @@mountainThreshold)
+      puts("It's kind of a mountain")
+      @mountainous = true
+      @berryBushes = BerryBush.new(berryBushes, 0, 0, true)
+    else
+      @mountainous = false
+      puts("It's not really a mountain")
+      @berryBushes = BerryBush.new(berryBushes)
+    end
+    
   end
 
   def getSpecies
@@ -21,6 +41,14 @@ class Land
 
   def getElevation
     return(@elevation)
+  end
+
+  def getBerryBushes
+    return(@berryBushes)
+  end
+
+  def isMountainous
+    return(@mountainous)
   end
 
   def to_s

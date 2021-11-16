1f5db2fc6eb2949d6281d3e394fd011e8b145022
taylor birchem <tbirchem@westmont.edu>
2021-11-15 16:14:20 -0800

    Window

--- main.rb
+++ main.rb
@@ -1,22 +1,60 @@
 #writen by Taylor Birchem (10/19/2021)
 #last modified by Taylor Birchem (11/11/2021)
 
+
+#writen by Taylor Birchem (10/19/2021)
+#last modified by Taylor Birchem (11/11/2021)
+
 require 'ruby2d'
 
 set background:'white'
 set width: 300, height: 200
 
-board = [
-  " "," "," "," ",
-  " "," "," "," ",
-  " "," "," "," ",
-  " "," "," "," ",
-  " "," "," "," ",
-  " "," "," "," ",
-  " "," "," "," ",
-  " "," "," "," ",]
+puts board = [
+  " "," "," "," "," "," "," "," ",
+  " "," "," "," "," "," "," "," ",
+  " "," "," "," "," "," "," "," ",
+  " "," "," "," "," "," "," "," ",
+  " "," "," "," "," "," "," "," ",
+  " "," "," "," "," "," "," "," ",
+  " "," "," "," "," "," "," "," ",
+  " "," "," "," "," "," "," "," ",
+]
+
+def display_board
+     puts row = ["   " "|" "   " "|" "   "]
+     puts separator = "-----------"
+     puts row
+     puts separator
+     puts row
+     puts separator
+     puts row
+     puts separator
+     puts row
+     puts separator
+     puts row
+     puts separator
+     puts row
+     puts separator
+     puts row
+     puts separator
+     puts row
+end
 
 display_board(board)
+
+class Bunnies
+  #input for bunnies
+    puts "How many bunnies would you like: "
+    bunnies = gets.chomp
+    puts(bunnies + " Bunnies")
+  end
+
+i = bunnies
+
+while i > 0
+ sort_by { rand }
+
 
 
 show
--- menu.rb
+++
@@ -1,9 +0,0 @@
-#writen by Taylor Birchem (10/19/2021)
-#last modified by Taylor Birchem (10/19/2021)
-
-class Bunnies
-  #input for bunnies
-    puts "How many bunnies would you like: "
-    bunnies = gets.chomp
-    puts(bunnies + " Bunnies")
-  end

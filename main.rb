df7cf9161362c593c9a9149b533fb2a884a1a83e
jchiplin <60380336+jchiplin@users.noreply.github.com>
2021-12-04 22:18:23 -0800

    Update main.rb
    Brought code previously from bottom of Population into main and allowed for grid dimension input

--- main.rb
+++ main.rb
@@ -1,58 +1,66 @@
-#writen by Taylor Birchem (10/19/2021)
-#last modified by Joshua Cross (11/6/2021)
-#writen by Taylor Birchem (10/19/2021)
-#last modified by Taylor Birchem (11/11/2021)
+#last modified by Joshua Cross (11/26/2021)
+#last modified by Jack Chiplin (12/4/2021)
+#require 'sinatra'
+require_relative 'test.rb'
+require_relative 'Grid'
+require_relative 'Population.rb'
 
-require 'ruby2d'
+#set :protection, :except => :frame_options
+#set :bind, '0.0.0.0'
+#set :port, 8080
+#Josh here, the next line is a call to my test code. It can be commented out when not testing my code
+#joshTestFunction
+#test = Grid.new(10,10,Array[40, 6])
 
-set background:'white'
-set width: 300, height: 200
+#Jack's test
+quitAnswer = ""
+puts "To quit the simulation, enter 'q'"
+puts "Enter height of ecostystem grid:"
+gridHeight = gets.chomp.to_i
 
-puts board = [
-  " "," "," "," "," "," "," "," ",
-  " "," "," "," "," "," "," "," ",
-  " "," "," "," "," "," "," "," ",
-  " "," "," "," "," "," "," "," ",
-  " "," "," "," "," "," "," "," ",
-  " "," "," "," "," "," "," "," ",
-  " "," "," "," "," "," "," "," ",
-  " "," "," "," "," "," "," "," ",
-]
-
-def display_board
-     puts row = ["   " "|" "   " "|" "   "]
-     puts separator = "-----------"
-     puts row
-     puts separator
-     puts row
-     puts separator
-     puts row
-     puts separator
-     puts row
-     puts separator
-     puts row
-     puts separator
-     puts row
-     puts separator
-     puts row
-     puts separator
-     puts row
+while(quitAnswer != 'q')
+  if(gridHeight != 'q')
+    puts "Enter width of ecosystem grid: (Expand window for larger widths)"
+    gridWidth = gets.chomp.to_i
+    if(gridWidth != 'q')
+      puts "Enter a starting population for bunnies:"
+      bunnyStartPopulation = gets.chomp.to_i
+      if(bunnyStartPopulation != 'q')
+        puts "Enter a starting population for wolves:"
+        wolfStartPopulation = gets.chomp.to_i
+        if (wolfStartPopulation != 'q')
+          population1 = Population.new(bunnyStartPopulation, wolfStartPopulation, gridHeight, gridWidth)
+          population1.appendStartingPopulation
+          answer = ""
+          while (answer != 'q')
+            puts "To progress a month enter 'Y' or 'y'"
+            answer = gets.chomp
+            if(answer != 'q')
+              population1.setCurrentPopulation
+              population1.progressMonth(answer)
+            elsif(answer == 'q')
+              quitAnswer = 'q'
+              puts "Quit simulation"
+            end
+          end
+        elsif(bunnyStartPopulation == 'q')
+          quitAnswer = 'q'
+          puts "Quit simulation"
+        end
+      elsif(wolfStartPopulation == 'q')
+        quitAnswer = 'q'
+        puts "Quit simulation"
+      end
+    elsif(gridWidth == 'q')
+      quitAnswer = 'q'
+      puts "Quit simulation"
+    end
+  elsif(gridHeight == 'q')
+    quitAnswer = 'q'
+    puts "Quit simulation"
+  end
 end
 
-display_board(board)
-
-class Bunnies
-  #input for bunnies
-    puts "How many bunnies would you like: "
-    bunnies = gets.chomp
-    puts(bunnies + " Bunnies")
-  end
-
-i = bunnies
-
-while i > 0
- sort_by { rand }
-
-
-
-show
+#get '/' do
+#  'Window that simulation will run in... This is a rough prototype and will be updated as project goes on.'
+#end
